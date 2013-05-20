18a4ca35d05edf4f73a4cae0a84d6ab478c539fc
David Hamilton <dham04ster@gmail.com>
2013-05-20 11:00:47 -0500

    Add birthday_deals.css to assets precompile for production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Birthdayclubjeffcity::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( dashboard.css )
+  config.assets.precompile += %w( dashboard.css birthday_deals.css )
 
   # Enable threaded mode
   # config.threadsafe!
